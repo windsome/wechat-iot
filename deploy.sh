f2acb4d60cf59eff088bd3ba3ea80a90c1e7eb3f
windsome <36081189@qq.com>
2016-03-11 16:32:07 +0800

    init version

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# This script builds demo and pushes it to gh-pages branch
+#
+
+# clean up
+cd "$(git rev-parse --show-toplevel)"
+set -e
+rm -rf demo/dist || exit 0;
+mkdir demo/dist;
+
+# build
+cd demo
+webpack
+cd dist
+
+# deploy
+git init
+git remote add origin https://github.com/windsome/weixin-site.git
+git add .
+git commit -m "Deploy to GitHub Pages"
+git checkout -b gh-pages
+git push -uf origin gh-pages
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: {
+    index: './src/index.jsx',
+    more: './src/more.jsx',
+    highstock: './src/highstock.jsx',
+    highmaps: './src/highmaps.jsx'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['react','es2015', 'stage-2']
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      "highcharts-more" : "highcharts/highcharts-more.src.js",
+      "react-highcharts": 'react-highcharts/dist'
+    },
+    modulesDirectories: ['node_modules']
+  },
+  output: {
+    path: 'dist/',
+    filename: '[name].js',
+    publicPath: '/'
+  }
+};
