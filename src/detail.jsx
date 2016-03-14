33953d181b68a92fa25e34191cfe50db5f809231
windsome <36081189@qq.com>
2016-03-14 14:33:49 +0800

    fix for solid gauge.

--- src/detail.jsx
+++ src/detail.jsx
@@ -2,24 +2,206 @@ import React, { Component, PropTypes } from 'react';
 import * as ReactDOM from 'react-dom';
 //import { ReactHighcharts } from 'react-highcharts';
 var ReactHighcharts = require('react-highcharts');
+var HighchartsMore = require('highcharts-more');
+// We tell HighchartsMore to use the same Highcharts object as ReactHighcharts
+HighchartsMore(ReactHighcharts.Highcharts);
+var HighchartsSolidGauge = require('highcharts/modules/solid-gauge');
+HighchartsSolidGauge(ReactHighcharts.Highcharts);
 
 var config = {
-  xAxis: {
-    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  /*chart: {
+    type: 'spline'
+  },*/
+  title: {
+    text: 'temperature line'
   },
+  xAxis: {
+    type: 'datetime',
+    dateTimeLabelFormats: { // don't display the dummy year
+      month: '%e. %b',
+      year: '%b'
+    },
+    title: {
+      text: 'Date'
+    }
+  },
+  yAxis: {
+    title: {
+      text: 'Temp(C)'
+    },
+    min: -30
+  },
+
   series: [{
-    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+    name: 'temp1',
+    data: [
+      [Date.UTC(1970, 9, 21), 1],
+      [Date.UTC(1970, 10, 4), 3.28],
+      [Date.UTC(1970, 10, 9), 0.25],
+      [Date.UTC(1970, 10, 27), 0.2],
+      [Date.UTC(1970, 11, 2), 0.28],
+      [Date.UTC(1970, 11, 26), 0.28],
+      [Date.UTC(1970, 11, 29), 0.47],
+      [Date.UTC(1971, 0, 11), 0.79],
+      [Date.UTC(1971, 0, 26), 0.72],
+      [Date.UTC(1971, 1, 3), 1.02],
+      [Date.UTC(1971, 1, 11), 1.12],
+      [Date.UTC(1971, 1, 25), 1.2],
+      [Date.UTC(1971, 2, 11), 1.18],
+      [Date.UTC(1971, 3, 11), 1.19],
+      [Date.UTC(1971, 4, 1), 1.85],
+      [Date.UTC(1971, 4, 5), 2.22],
+      [Date.UTC(1971, 4, 19), 1.15],
+      [Date.UTC(1971, 4, 30), 0]
+    ]
   }]
 };
 
+    var gaugeOptions = {
+        chart: {
+            type: 'solidgauge'
+        },
+        title: null,
+        pane: {
+            center: ['50%', '85%'],
+            size: '140%',
+            startAngle: -90,
+            endAngle: 90,
+            background: {
+                backgroundColor: (ReactHighcharts.theme && ReactHighcharts.theme.background2) || '#EEE',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
+            }
+        },
+        tooltip: {
+            enabled: false
+        },
+        // the value axis
+        yAxis: {
+            stops: [
+                [0.1, '#55BF3B'], // green
+                [0.5, '#DDDF0D'], // yellow
+                [0.9, '#DF5353'] // red
+            ],
+            lineWidth: 0,
+            minorTickInterval: null,
+            tickPixelInterval: 400,
+            tickWidth: 0,
+            title: {
+                y: -70
+            },
+            labels: {
+                y: 16
+            }
+        },
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 5,
+                    borderWidth: 0,
+                    useHTML: true
+                }
+            }
+        }
+    };
+
+    var gauge1Options = {
+        yAxis: {
+            min: 0,
+            max: 200,
+            title: {
+                text: 'Speed'
+            }
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        series: [{
+            name: 'Speed',
+            data: [80],
+            dataLabels: {
+                format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                    ((ReactHighcharts.theme && ReactHighcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                       '<span style="font-size:12px;color:silver">km/h</span></div>'
+            },
+            tooltip: {
+                valueSuffix: ' km/h'
+            }
+        }]
+
+    };
+    
+var globe_day = 0;
+
 class PageDetailInfo extends Component {
-  componentDidMount() {
+  setInterval() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  }
+  componentWillMount() {
+    this.intervals = [];
+  }
+  componentWillUnmount() {
+    this.intervals.map(clearInterval);
+    this.serverRequest.abort();
+  }
+  /*componentDidMount() {
     let chart = this.refs.chart.getChart();
-    chart.series[0].addPoint({x: 'dec1', y: 100});
-    chart.series[0].addPoint({x: 'dec2', y: 150});
-    chart.series[0].addPoint({x: 'dec3', y: 200});
+    chart.series[0].addPoint({x: Date.UTC(1971, 5, 10), y: 10});
+    chart.series[0].addPoint({x: Date.UTC(1971, 5, 15), y: 15});
+    chart.series[0].addPoint({x: Date.UTC(1971, 5, 20), y: 20});
+  }*/
+  addPoint() {
+    let chart = this.refs.chart.getChart();
+
+    let sed = Math.random();
+    let val = Math.round(sed * 20);
+    let month = Math.round(sed * 12);
+    let day = Math.round(sed * 30);
+    console.log ("val="+val+", month="+month+",day="+day);
+
+    //chart.series[0].addPoint({x: Date.UTC(1971, month, day), y: val});
+    chart.series[0].removePoint(0);
+    chart.series[0].addPoint({x: Date.UTC(1971, 5, globe_day++), y: val});
+  }
+  changeGuage () {
+    let chart = this.refs.gauge1.getChart();
+        var point,
+            newVal,
+            inc;
+
+        if (chart) {
+            point = chart.series[0].points[0];
+            inc = Math.round((Math.random() - 0.5) * 100);
+            newVal = point.y + inc;
+
+            if (newVal < 0 || newVal > 200) {
+                newVal = point.y - inc;
+            }
+
+            point.update(newVal);
+        }
+  }
+  componentDidMount() {
+    this.setInterval(this.addPoint.bind(this), 2000);
+    this.setInterval(this.changeGuage.bind(this), 2000);
+    //let url = this.props.source;
+    //let url = "https://api.github.com/_private/browser/stats"
+    //let url = "https://avatars1.githubusercontent.com/u/226573?v=3&s=40"
+    let url = "https://assets-cdn.github.com/assets/frameworks-2d635ad8ba43f32e07aa1fd713d692eff9a2e78dcd63daa5b580c38b36df3fdb.css"
+    this.serverRequest = $.get(url, function (result) {
+      /*var lastGist = result[0];
+      this.setState({
+        username: lastGist.owner.login,
+        lastGistUrl: lastGist.html_url
+      });*/
+      console.log("result="+result);
+    }.bind(this));
   }
   render() {
+    var gauge1 = Object.assign ({}, gaugeOptions, gauge1Options);
     return (
       <div>
         <ol className="breadcrumb">
@@ -27,7 +209,10 @@ class PageDetailInfo extends Component {
           <li className="active">设备详情（大棚1）</li>
         </ol>
         <div>
-          <img src="{:FARM_TEMPLETE_PATH}/images/1.jpg" className="img-responsive" alt="Responsive image"/>
+          <img src="1.jpg" className="img-responsive" alt="Responsive image"/>
+        </div>
+        <div id="gauge1" >
+          <ReactHighcharts config={gauge1} ref="gauge1"></ReactHighcharts>
         </div>
         <div id="charts1" >
           <ReactHighcharts config={config} ref="chart"></ReactHighcharts>
@@ -45,3 +230,4 @@ ReactDOM.render(
 
 require("file?name=[name].[ext]!./detail.html");
 require("file?name=[name].[ext]!./charts.css");
+require("file?name=[name].[ext]!./1.jpg");
